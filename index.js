d42208d7e8f6239187e13f3278cc645d2c09ebcb
Nathan Leggatt <nathan@abendago.com>
2024-09-26 16:43:34 -0700

    Add output log

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
+    console.log("received a request")
     res.json({
         status: "online",
         data: [
